7ae226d56222738b7de364ca58ef375a4066aa1f
Abbey Ricker <rickeraec23@gmail.com>
2023-03-18 11:51:39 -0500

    workshop 1 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import Header from "./components/Header";
 import "./App.css";
+import CampsitesList from "./features/campsites/CampsitesList";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Nucamp!</h1>
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
