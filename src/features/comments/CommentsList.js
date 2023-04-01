13b8cb330adf75d31cbfd4483aef50963914ca57
Abbey Ricker <rickeraec23@gmail.com>
2023-04-01 09:55:04 -0500

    week 3 workshop ternary

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -5,18 +5,15 @@ import CommentForm from "./CommentForm";
 
 const CommentsList = ({ campsiteId }) => {
   const comments = selectCommentsByCampsiteId(campsiteId);
-  if (comments && comments.length > 0) {
-    return (
-      <Col md="5" className="m-1">
-        <h4>Comments</h4>
-        {comments.map((comment) => {
-          return <Comment key={comment.id} comment={comment} />;
-        })}
-        <CommentForm campsiteId={campsiteId} />
-      </Col>
-    );
-  }
-  return (
+  return comments && comments.length > 0 ? (
+    <Col md="5" className="m-1">
+      <h4>Comments</h4>
+      {comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment} />;
+      })}
+      <CommentForm campsiteId={campsiteId} />
+    </Col>
+  ) : (
     <Col md="5" className="m-1">
       There are no comments for this campsite yet.
     </Col>
