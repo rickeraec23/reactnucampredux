576d7f1e8a1e50ddc609eac5bec7cbd522fd8f15
rickeraec23 <124188518+rickeraec23@users.noreply.github.com>
2023-04-18 08:07:23 -0500

    workshop 5 React 2nd

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,8 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { COMMENTS } from "../../app/shared/COMMENTS";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { baseUrl } from "../../app/shared/baseUrl";
+import { mapImageURL } from "../../utils/mapImageURL";
+
+export const fetchComments = createAsyncThunk(
+  'comments/fetchComments',
+  async () => {
+      const response = await fetch(baseUrl + 'comments');
+      if (!response.ok) {
+          return Promise.reject('Unable to fetch, status: ' + response.status);
+      }
+      const data = await response.json();
+      return data;
+  }
+);
+
+export const postComment = createAsyncThunk(
+  'comments/postComment',
+  async (comment, { dispatch }) => {
+    const response = await fetch(baseUrl + 'comments', {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      return Promise.reject(response.status);
+    }
+    const data = await response.json();
+    dispatch(addComment(data));
+  }
+);
 
 const initialState = {
-  commentsArray: COMMENTS,
+  commentsArray: [],
+  isLoading: true,
+  errMsg: '',
 };
 
 const commentsSlice = createSlice({
@@ -10,8 +43,6 @@ const commentsSlice = createSlice({
   initialState,
   reducers: {
     addComment: (state, action) => {
-      console.log("addComment action.payload:", action.payload);
-      console.log("addComment state.commentsArray:", state.commentsArray);
       const newComment = {
         id: state.commentsArray.length + 1,
         ...action.payload,
@@ -19,6 +50,23 @@ const commentsSlice = createSlice({
       state.commentsArray.push(newComment);
     },
   },
+  extraReducers: {
+    [fetchComments.pending]: (state) => {
+      state.isLoading = true;
+  },
+  [fetchComments.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = '';
+      state.commentsArray = mapImageURL(action.payload);
+  },
+  [fetchComments.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = action.error ? action.error.message : 'Fetch failed';
+  },
+  [postComment.rejected]: (state, action) => {
+      alert(`Unable to post comment. Error status: ${action.payload}`);
+  }
+  }
 });
 
 export const commentsReducer = commentsSlice.reducer;
